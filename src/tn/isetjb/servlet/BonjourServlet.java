5cf61e0794eef971675e9dcf13a65aa5b4aaca02
ISET Djerba <ti.isetjb@gmail.com>
2020-03-03 16:20:05 +0100

    Créer et déployer une première Servlet avec Tomcat

---
+++ src/tn/isetjb/servlet/BonjourServlet.java
@@ -0,0 +1,38 @@
+package tn.isetjb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BonjourServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\" />");
+        out.println("<title>Bonjour Iset</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>Bonjour Iset Djerba!</p>");
+        out.println("</body>");
+        out.println("</html>");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	
+}
